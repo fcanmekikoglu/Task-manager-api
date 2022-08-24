527c58eb324bc7ce08e16a270f153d93d8120bf5
Fahrettin Can Mekikoğlu <fcanmekikoglu@gmail.com>
2022-08-24 19:39:19 +0300

    added test cases for user login

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,21 @@
 const request = require ('supertest')
 const app =  require('../src/app')
+const User = require('../src/models/user')
 
+//Create valid user
+const userOne = {
+    name: "Meltem",
+    email: "meltem@mail.com",
+    password: '123What!!!!!'
+}
+
+//Delete everything before every test case and save valid user to db
+beforeEach(async ()=>{
+    await User.deleteMany()
+    await new User(userOne).save()    
+})
+
+//Signup test with valid user
 test('Should signup a new user', async ()=>{
     await request(app).post('/users').send({
         name: 'Can mekikoglu',
@@ -8,4 +23,19 @@ test('Should signup a new user', async ()=>{
         password: 'Sifrem123123'
     }).expect(201)
 })
-    
+
+//Login test with valid user
+test('Should login existing user', async()=>{
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+//Login test with non-valid credentials is expected to crash (400) 
+test('Should not login with not existing user', async()=>{
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: "nonExistentPass123!"
+    }).expect(400)
+})
